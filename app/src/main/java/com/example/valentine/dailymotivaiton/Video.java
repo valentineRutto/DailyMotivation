093bc1f8f6d0d0d937a44097e272ff67199639b3
valentinerutto <rutto28@gmail.com>
2015-10-13 23:14:22 +0300

    added cardview layout file and recyclerview on video class

--- app/src/main/java/com/example/valentine/dailymotivaiton/Video.java
+++ app/src/main/java/com/example/valentine/dailymotivaiton/Video.java
@@ -1,11 +1,13 @@
 package com.example.valentine.dailymotivaiton;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class Video extends AppCompatActivity {
 String tag="Video";
@@ -14,6 +16,18 @@ String tag="Video";
         super.onCreate(savedInstanceState);
         Log.i(tag, "onCreate");
         setContentView(R.layout.activity_video);
+
+        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.my_recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+//Add8cards
+
+        MyAdapter adapter=new MyAdapter(new String[8]);
+
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -63,5 +77,25 @@ String tag="Video";
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter {
+        public MyAdapter(String[] strings) {
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
